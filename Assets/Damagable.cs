28372747087f5ee47a3398462a137bfd0043087f
Osman Pekaydın <pekaydin580@gmail.com>
2020-10-19 03:00:34 +0300

    Initial commit

---
+++ Assets/Damagable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class Damagable : MonoBehaviour, ITakeDamage
+{
+    [SerializeField] private int maxHealth;
+
+    private int health;
+
+    private void Awake ()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/DamageOnClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnClick : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            //sahnedeki Damagable'dan kalıtım almış tüm objelere hasar verir
+            //var damagebles = FindObjectsOfType<Damagable>();
+            //foreach (var damagable in damagebles)
+            //{
+            //    damagable.TakeDamage(1);
+            //}
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                ITakeDamage damagable = hitInfo.collider.GetComponent<ITakeDamage>();
+                if (damagable != null)
+                {
+                    damagable.TakeDamage(1);
+                }
+            }
+        }
+    }
+}
